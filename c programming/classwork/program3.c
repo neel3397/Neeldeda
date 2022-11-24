311109ffb2ff3a28ad98deffb2205dcb93ebeacb
neel3397 <neeldeda7@gmail.com>
2022-11-24 11:35:10 +0530

    a

--- c programming/classwork/program3.c
+++ c programming/classwork/program3.c
@@ -3,5 +3,8 @@
 void main()
 {
 	printf("first name: neel\n");
-	printf("last name: deda");
+	printf("last name: deda\n");
+	printf("mobile no: 9265539153\n");
+	printf("birthdate: 3-3-1997\n");
+	
 }
---
+++ c programming/classwork/program4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+	int n1=34;
+	int n2=23;
+	
+	printf("a = %d\n",n1);
+	printf("b = %d\n",n2);
+	
+}
